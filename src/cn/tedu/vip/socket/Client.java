99411a9f4d1f9e652a28f99b5d118bd7cb9d7150
qitianming <2318161731@qq.com>
2022-10-17 12:17:36 +0800

    JAVASE

--- src/cn/tedu/vip/socket/Client.java
+++ src/cn/tedu/vip/socket/Client.java
@@ -3,59 +3,58 @@ package cn.tedu.vip.socket;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Client {
-    private Socket socket;
+	private Socket socket;
 
-    public Client() {
-        try {
-            /*
-            实锟斤拷锟斤拷Socket锟斤拷要锟斤拷锟斤拷锟斤拷锟斤拷
-            1.锟斤拷锟斤拷锟斤拷锟斤拷IP锟斤拷址
-            2.锟斤拷锟斤拷锟斤拷锟斤拷,锟剿口猴拷
+	public Client() {
+		try {
+			/*
+			 * 实锟斤拷锟斤拷Socket锟斤拷要锟斤拷锟斤拷锟斤拷锟斤拷 1.锟斤拷锟斤拷锟斤拷锟斤拷IP锟斤拷址 2.锟斤拷锟斤拷锟斤拷锟斤拷,锟剿口猴拷
+			 * 
+			 * 只要实锟斤拷锟斤拷锟缴癸拷锟斤拷锟斤拷锟斤拷锟接成癸拷 锟斤拷锟绞э拷芫突锟斤拷锟斤拷锟届常
+			 * 
+			 * 锟剿口猴拷锟角硷拷锟斤拷锟斤拷峁╋拷锟斤拷锟侥接口的憋拷锟斤拷 ip锟斤拷址锟角硷拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷械锟斤拷锟斤拷锟轿伙拷锟�
+			 */
+			System.out.println("开始连接");
+			socket = new Socket("localhost", 8080);
+			System.out.println("连接成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		/*
+		 * win+R锟斤拷锟斤拷cmd 锟节碉拷锟斤拷锟侥斤拷锟斤拷锟斤拷锟斤拷锟斤拷ipconfig锟桔察本锟斤拷锟斤拷ip锟斤拷址
+		 */
+	}
 
-            只要实锟斤拷锟斤拷锟缴癸拷锟斤拷锟斤拷锟斤拷锟接成癸拷
-            锟斤拷锟绞э拷芫突锟斤拷锟斤拷锟届常
+	public void start() {
+		/*
+		 * 要锟诫发锟斤拷锟斤拷息,锟斤拷要锟斤拷socket锟斤拷锟斤拷锟叫伙拷锟斤拷锟斤拷锟斤拷 OutputStream getOutStream;
+		 */
+		try {
+			OutputStream os = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+			BufferedWriter bw = new BufferedWriter(osw);
+			PrintWriter pw = new PrintWriter(bw, true);
 
-            锟剿口猴拷锟角硷拷锟斤拷锟斤拷峁╋拷锟斤拷锟侥接口的憋拷锟斤拷
-            ip锟斤拷址锟角硷拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷械锟斤拷锟斤拷锟轿伙拷锟�
-             */
-            System.out.println("开始连接");
-            socket = new Socket("localhost", 8080);
-            System.out.println("连接成功");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        /*
-        win+R锟斤拷锟斤拷cmd
-        锟节碉拷锟斤拷锟侥斤拷锟斤拷锟斤拷锟斤拷锟斤拷ipconfig锟桔察本锟斤拷锟斤拷ip锟斤拷址
-         */
-    }
+			Scanner scan = new Scanner(System.in);
+			while (true) {
+				String msg = scan.nextLine();
+				pw.println(msg);
+			}
 
-    public void start() {
-        /*
-        要锟诫发锟斤拷锟斤拷息,锟斤拷要锟斤拷socket锟斤拷锟斤拷锟叫伙拷锟斤拷锟斤拷锟斤拷
-        OutputStream getOutStream;
-         */
-        try {
-            OutputStream os = socket.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-            BufferedWriter bw = new BufferedWriter(osw);
-            PrintWriter pw = new PrintWriter(bw, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-            pw.println("你好!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        /*
-         * java.net.Socket
-         * Socket锟斤拷装锟斤拷TCP通锟斤拷细锟斤拷,锟斤拷锟斤拷只锟斤拷要锟斤拷确IP锟斤拷址锟酵端口猴拷
-         * 锟斤拷锟斤拷锟斤拷锟斤拷朔锟斤拷锟斤拷锟斤拷锟斤拷(锟斤拷锟斤拷锟斤拷)锟斤拷锟斤拷
-         */
-        Client client = new Client();
-        client.start();
-    }
+	public static void main(String[] args) {
+		/*
+		 * java.net.Socket Socket锟斤拷装锟斤拷TCP通锟斤拷细锟斤拷,锟斤拷锟斤拷只锟斤拷要锟斤拷确IP锟斤拷址锟酵端口猴拷
+		 * 锟斤拷锟斤拷锟斤拷锟斤拷朔锟斤拷锟斤拷锟斤拷锟斤拷(锟斤拷锟斤拷锟斤拷)锟斤拷锟斤拷
+		 */
+		Client client = new Client();
+		client.start();
+	}
 }
--- src/cn/tedu/vip/socket/Server.java
+++ src/cn/tedu/vip/socket/Server.java
@@ -10,56 +10,56 @@ import java.nio.charset.StandardCharsets;
 
 public class Server {
 
-    /**
-     * java.net.ServerSocket
-     * 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟侥讹拷锟斤拷
-     * 1.锟斤拷锟斤拷锟斤拷锟剿匡拷
-     * 2.锟杰癸拷锟斤拷锟斤拷锟酵伙拷锟剿凤拷锟酵癸拷锟斤拷锟斤拷锟斤拷息
-     */
-    private ServerSocket server;
+	/**
+	 * java.net.ServerSocket 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟侥讹拷锟斤拷 1.锟斤拷锟斤拷锟斤拷锟剿匡拷
+	 * 2.锟杰癸拷锟斤拷锟斤拷锟酵伙拷锟剿凤拷锟酵癸拷锟斤拷锟斤拷锟斤拷息
+	 */
+	private ServerSocket server;
 
-    public Server() {
-        try {
-            /*
-            实锟斤拷锟斤拷ServerSocket锟斤拷锟斤拷
-            锟斤拷锟斤拷锟较低筹拷锟斤拷锟剿匡拷
-            锟斤拷锟斤拷锟斤拷锟剿口憋拷占锟矫会发锟斤拷锟届常
-             */
-            System.out.println("服务器正在启动");
-            server = new ServerSocket(8080);
-            System.out.println("服务器启动完毕");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public Server() {
+		try {
+			/*
+			 * 实锟斤拷锟斤拷ServerSocket锟斤拷锟斤拷 锟斤拷锟斤拷锟较低筹拷锟斤拷锟剿匡拷 锟斤拷锟斤拷锟斤拷锟剿口憋拷占锟矫会发锟斤拷锟届常
+			 */
+			System.out.println("服务器正在启动");
+			server = new ServerSocket(8080);
+			System.out.println("服务器启动完毕");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void start() {
-        /*
-        Socket accept()
-        锟矫凤拷锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷,锟斤拷锟矫猴拷,锟斤拷锟斤拷锟斤拷停锟饺达拷锟酵伙拷锟剿碉拷锟斤拷息锟斤拷锟酵癸拷锟斤拷
-        没锟斤拷锟斤拷息锟斤拷锟斤拷锟斤拷一直锟斤拷
-         */
-        try {
-            System.out.println("等待客户端连接");
-            Socket socket = server.accept();
-            System.out.println("一个客户端连接了");
-            /*
-            锟斤拷socket锟叫伙拷锟斤拷锟斤拷锟斤拷锟�
-            InputStream getInputStream()
-             */
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-            BufferedReader br = new BufferedReader(isr);
+	public void start() {
+		/*
+		 * Socket accept()
+		 * 锟矫凤拷锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷,锟斤拷锟矫猴拷,锟斤拷锟斤拷锟斤拷停锟饺达拷锟酵伙拷锟剿碉拷锟斤拷息锟斤拷锟酵癸拷锟斤拷
+		 * 没锟斤拷锟斤拷息锟斤拷锟斤拷锟斤拷一直锟斤拷
+		 */
+		try {
+			System.out.println("等待客户端连接");
+			Socket socket = server.accept();
+			System.out.println("一个客户端连接了");
+			/*
+			 * 锟斤拷socket锟叫伙拷锟斤拷锟斤拷锟斤拷锟� InputStream getInputStream()
+			 */
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+			BufferedReader br = new BufferedReader(isr);
+			/*
+			 * 这里需要使用下面的方式来读取客户端发送过来的内容 br.readLine()这个方法在不同系统下运行时会有不同的反应
+			 * windows系统,客户端断开会发生异常 linux系统客户端断开返回null,当返回null时保证循环能够结束
+			 */
+			String msg;
+			while ((msg = br.readLine()) != null) {
+				System.out.println(msg);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-            String msg = br.readLine();
-            System.out.println("msg");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
-    }
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.start();
+	}
 }
